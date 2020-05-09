912e92916c2d5ad3ee99ccc73069e6e4190a1295
Junya Oiwa <0920oiwa@gmail.com>
2020-05-10 01:25:37 +0900

    [add]URLからデータを保存するのを実装

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const axios = require('axios');
+const sanitizeFilename = require('sanitize-filename');
 
 async function main() {
   const file = process.argv[2];
@@ -10,8 +11,31 @@ async function main() {
   }
   console.log(`${file}を読み込みます。`);
   const url_list = await readUrlList(file);
-  // console.log(url_list);
+  console.log(url_list);
   console.log(`${url_list.length}件のURLを読み込みました。`);
+
+  // 一次保存先フォルダがなければdlフォルダを作る
+  if (!fs.existsSync('./dl/')) {
+    fs.mkdirSync('./dl/');
+  };
+
+  // 実行
+  for(item in url_list) {
+    const file_name = sanitizeFilename(url_list[item], {replacement: '-'}).replace(/\./g, '-') + '.txt';
+    const path = './dl/';
+    const fetch_data = await fetchUrl(url_list[item]);
+    fs.readFile(path + file_name, 'utf-8', (err, data) => {
+      if(err) {
+        fs.writeFile(path + file_name, fetch_data, 'utf-8', err => {
+          if(err) {
+            console.log(err);
+          }
+        })
+      }
+    })
+    console.log(file_name);
+    // await fetchUrl(url_list[item]);
+  }
 }
 
 function readUrlList(file) {
@@ -20,8 +44,17 @@ function readUrlList(file) {
       if(err) { throw err; };
       const url_list = data.split(/\r\n|\r|\n/);
       resolve(url_list);
-    })
-  }) 
+    });
+  });
+}
+
+function fetchUrl(url) {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        resolve(response.data);
+      })
+  })
 }
 
 main();
