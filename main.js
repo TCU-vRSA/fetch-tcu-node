66e2c167c45efbb5c98417da2f8a816ec3c59cfb
Junya Oiwa <0920oiwa@gmail.com>
2020-05-10 02:55:21 +0900

    [add]とりあえず差分取ってくるところまでできた

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const axios = require('axios');
 const sanitizeFilename = require('sanitize-filename');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const diff = require('diff');
 
 async function main() {
   const file = process.argv[2];
@@ -27,14 +30,18 @@ async function main() {
     fs.readFile(path + file_name, 'utf-8', (err, data) => {
       if(err) {
         fs.writeFile(path + file_name, fetch_data, 'utf-8', err => {
-          if(err) {
-            console.log(err);
-          }
+          if(err) { throw err; };
         })
       }
-    })
-    console.log(file_name);
-    // await fetchUrl(url_list[item]);
+      else {
+        // fs.writeFile(path + 'new_' + file_name, fetch_data, 'utf-8', err => {
+        //   if(err) { throw err; };
+        // })
+        const diffs = diffContent(data, fetch_data);
+        postDiscord(url_list[item], diffs);
+
+      }
+    });
   }
 }
 
@@ -55,6 +62,48 @@ function fetchUrl(url) {
         resolve(response.data);
       })
   })
+}
+
+function diffContent(before, after) {
+  const before_dom = new JSDOM(before);
+  const after_dom = new JSDOM(after);
+  const before_body = before_dom.window.document.body.outerHTML;
+  const after_body = after_dom.window.document.body.outerHTML;
+
+  result = diff.diffLines(before_body, after_body);
+  let diffs = [];
+  result.forEach(item => {
+    if(item.added) {
+      diffs.push('追記: ' + item.value);
+    }
+    else if(item.removed) {
+      diffs.push('削除: ' + item.value);
+    }
+  });
+
+  return diffs;
+}
+
+function postDiscord(url, contents) {
+  const tmp = {
+    "username": "TCU変更通知Bot",
+    "avatar_url": "https://pbs.twimg.com/profile_images/1250820091018539008/4uztlH6f_400x400.jpg",
+    "content": `${url}に変更がありました。`,
+    "embeds": [
+      {
+        "fields": []
+      }
+    ]
+  };
+  contents.forEach(item => {
+    const t = {
+      "name": "変更点",
+      "value": item
+    }
+    tmp.embeds[0].fields.push(t);
+  })
+  
+  console.log(tmp);
 }
 
 main();
